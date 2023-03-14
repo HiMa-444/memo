56a45b042a751626057e8a53350243b10f16aee6
EC2 Default User <ec2-user@ip-172-31-20-214.ap-northeast-1.compute.internal>
2023-03-14 19:45:37 +0000

    提出

---
+++ memo_kadai.rb
@@ -0,0 +1,25 @@
+require 'csv'
+while 1 == 1 do
+  puts "1:メモの新規作成,2:ファイルの編集,3:終了"
+  mode = gets.to_i
+  case mode 
+  when 1
+    puts "ファイル名を入力して下さい"
+    file_name = gets.chomp
+    puts "内容を記述して下さい(control + dで終了)"
+    input = readlines
+    memo = input
+    CSV.open("#{file_name}.csv",'w') do |csv|
+    csv.puts ["#{memo}"]
+    end
+  when 2
+    puts "ファイル名を入力して下さい"
+    file_name = gets.chomp
+    puts "内容を記述して下さい(control + dで終了)"
+    input = readlines
+    memo = input
+    CSV.open("#{file_name}.csv",'a') do |csv|
+    csv.puts ["#{memo}"]
+    end
+  end
+end
